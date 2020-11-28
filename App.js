bcd49ed90e14ee6a6c36e82a55175050d13b34f9
Kevin Luo <kvn.luo@gmail.com>
2020-11-28 12:33:17 -0500

    file structure for reviews app

--- App.js
+++ App.js
@@ -1,56 +1,9 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
+import React from "react";
 
-import TodoHeader from "./projects/todo/TodoHeader";
-import TodoForm from "./projects/todo/TodoForm";
-import TodoList from "./projects/todo/TodoList";
-import Sandbox from './projects/todo/sandbox';
+import Home from "./projects/reviewsApp/screens/Home";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create an app", key: "2" },
-    { text: "play on the switch", key: "3" },
-  ]);
-
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.key !== key);
-    });
-  };
-
   return (
-    // <Sandbox />
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <TodoHeader />
-        <View style={styles.content}>
-          <TodoForm setTodos={setTodos} />
-          <View style={styles.list}>
-            <TodoList pressHandler={pressHandler} todos={todos} />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  content: {
-    flex: 1,
-    padding: 40,
-  },
-  list: {
-    marginTop: 20,
-    flex: 1,
-  },
-});
---
+++ projects/todoApp/TodoApp.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+import TodoHeader from "./TodoHeader";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import Sandbox from './sandbox';
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key !== key);
+    });
+  };
+
+  return (
+    // <Sandbox />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <TodoHeader />
+        <View style={styles.content}>
+          <TodoForm setTodos={setTodos} />
+          <View style={styles.list}>
+            <TodoList pressHandler={pressHandler} todos={todos} />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    flex: 1,
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+    flex: 1,
+  },
+});
