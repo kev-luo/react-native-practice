5e87acb14db6eb6e14b97ff46a4baa5631b69f3c
Kevin Luo <kvn.luo@gmail.com>
2020-11-28 15:05:41 -0500

    sending data when navigating between screens

--- projects/reviewsApp/screens/Home.js
+++ projects/reviewsApp/screens/Home.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Button, View, Text } from 'react-native';
+import React, { useState } from "react";
+import { FlatList, View, Text, TouchableOpacity } from "react-native";
 
-import { globalStyles } from '../styles/global';
+import { globalStyles } from "../styles/global";
+import { data } from "../data";
 
 export default function Home({ navigation }) {
-  // all Screen components within our Navigator are passed a prop called navigation. we can use navigation.navigate() to go to a defined Screen in our Navigator. 
-  // the argument we pass into navigation.navigate() must match the name property of the Screen component
-  const pressHandler = () => {
-    navigation.navigate("Reviews");
-  }
+  const [reviews, setReviews] = useState(data);
+
+  // all Screen components within our Navigator are passed a prop called navigation. we can use navigation.navigate() to go to a defined Screen in our Navigator.
+  // the first argument we pass into navigation.navigate() must match the name property of the Screen component
+  // the second argument we pass into navigation.navigate() are additional information we want to pass to the navigated-to component
 
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title="Go to review Details" onPress={pressHandler}/>
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate("Reviews", item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
-  )
+  );
 }
--- projects/reviewsApp/screens/ReviewDetails.js
+++ projects/reviewsApp/screens/ReviewDetails.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
 
-import { globalStyles } from '../styles/global';
+import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails({navigation}) {
-
+export default function ReviewDetails({ route, navigation }) {
+  console.log(route);
   const handlePress = () => {
     // this "pops" a Screen off the stack and goes back one Screen
     navigation.goBack();
-  }
+  };
 
   return (
     <View style={globalStyles.container}>
-      <Text>Reviews</Text>
-      <Button title="Pop off" onPress={handlePress} />
+      <Text style={globalStyles.titleText}>Reviews</Text>
+      
     </View>
-  )
+  );
 }
