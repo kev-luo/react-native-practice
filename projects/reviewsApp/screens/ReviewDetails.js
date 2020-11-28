592c3cc66c16f26a7ae9a102d63e496afcd1f555
Kevin Luo <kvn.luo@gmail.com>
2020-11-28 18:21:13 -0500

    dynamic images based on game rating

--- projects/reviewsApp/screens/ReviewDetails.js
+++ projects/reviewsApp/screens/ReviewDetails.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 import Card from "../shared/Card";
-import { globalStyles } from "../styles/global";
+import { globalStyles, images } from "../styles/global";
 
 export default function ReviewDetails({ route: { params }, navigation }) {
   const { body, key, rating, title } = params;
@@ -16,8 +16,22 @@ export default function ReviewDetails({ route: { params }, navigation }) {
       <Card>
         <Text>{title}</Text>
         <Text>{body}</Text>
-        <Text>{rating}</Text>
+        <View style={styles.rating}>
+          <Text>Rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  },
+});
